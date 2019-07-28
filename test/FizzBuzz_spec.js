03ad3b0c6282c3d361cb0be86333db5956dc5b7c
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2019-07-28 23:05:39 +0800

    done with css and bootstrap decorations

---
+++ test/FizzBuzz_spec.js
@@ -0,0 +1,19 @@
+var should = chai.should()
+describe('function fizzBuzz', function () {
+  it('若該整數能被 3 整除，回傳 Fizz', function () {
+    var result = fizzBuzz(6)
+    result.should.be.equal('Fizz')
+  })
+  it('若該整數能被 5 整除，回傳 Buzz', function () {
+    var result = fizzBuzz(10)
+    result.should.be.equal('Buzz')
+  })
+  it('若該整數能被 3 和 5 同時整除，回傳 FizzBuzz', function () {
+    var result = fizzBuzz(60)
+    result.should.be.equal('FizzBuzz')
+  })
+  it('若都不能整除，回傳該整數', function () {
+    var result = fizzBuzz(1)
+    result.should.be.equal(1)
+  })
+})
